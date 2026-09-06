83be574354e4a96d8764e4150a868a01eeb4306e
Shridhar Gaikwad <shridhargaikwad333@gmail.com>
2026-09-06 20:14:48 +0530

    "update() and findByMobileNumber() added in CustomerRepository"

--- retailbank-customer-service/src/main/java/org/retailbank360/repository/CustomerRepository.java
+++ retailbank-customer-service/src/main/java/org/retailbank360/repository/CustomerRepository.java
@@ -59,4 +59,8 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select c from Customer c where c.id = :id")
     Optional<Customer> findByIdForUpdate(@Param("id") Long id);
+
+    Customer update(Customer customer);
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
